3dd9c933b561476401fcd605c6696233ab4fb370
zijun <zijunzhou06@gmai.com>
2024-10-05 14:04:35 -0400

    added Illness class and implemented getting ill in game

--- classes.py
+++ classes.py
@@ -1,9 +1,12 @@
+from enum import Enum
+
+
 class Game:
     def __init__(self, start_date="2024-01-01"):
         self.date = start_date
 
-        self.pollution_level = 0            # FROM 0 TO 5
-        self.government_sensitivity = 0     # FROM 0 TO 5
+        self.pollution = 0            # FROM 0 TO 10
+        self.government_sensitivity = 0     # FROM 0 TO 10
 
     def game_over(self):
         return
@@ -18,6 +21,7 @@ class Player:
         self.wage = 2000
         self.protests_attended = 0
         self.starving = False
+        self.illnesses = set()
 
     def stays_home(self):
         self.health += 3
@@ -35,11 +39,35 @@ class Player:
     def is_alive(self):
         return self.health > 0
 
+    def apply_illness_affects(self):
+        illnesses = self.illnesses
+
+        if Illness.IRRITATION in illnesses:
+            self.health -= 5
+        if Illness.HEADACHES in illnesses:
+            self.wage *= 0.95
+        if Illness.BREATHING_PROBLEMS in illnesses:
+            self.health -= 10
+            self.wage *= 0.95
+
 
 class Newspaper:
     def __init__(self, date, local_news, poster):
         self.date = date
         self.local_news = local_news
         self.poster = poster
+
+
+class Illness(Enum):
+    HEADACHES = 1
+    BREATHING_PROBLEMS = 2
+    IRRITATION = 3
+
+    ASTHMA = 4
+    CARDIOVASCULAR_DISEASE = 5
+
+    CANCER = 6
+    STROKE = 7
+    PNEUMONIA = 8
 
 
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import random
 from classes import *
+
+game = Game()
+player = Player()
+
+play_game = True
+
+while play_game:
+    if not player.is_alive():
+        game.game_over()
+
+    if 0 <= game.pollution < 3:
+        illness = random.choice([Illness.IRRITATION, Illness.HEADACHES, Illness.BREATHING_PROBLEMS])
+    elif 3 <= game.pollution < 6:
+        illness = random.choice([Illness.ASTHMA, Illness.CARDIOVASCULAR_DISEASE])
+    else:
+        illness = random.choice([Illness.CANCER, Illness.PNEUMONIA, Illness.STROKE])
+
+    player.illnesses.add(illness)
+    player.apply_illness_affects()
+
+    # show newspaper
+    # player decides to go to work, stay home, or protest (or must go to hospital)
+
