827d04e02631dba0609747a629ec4e8db744d9a9
zijun <zijunzhou06@gmai.com>
2024-10-05 12:29:56 -0400

    Expanded player class

--- classes.py
+++ classes.py
@@ -1,37 +1,45 @@
 class Game:
-    def __init__(self, date="2024-01-01"):
-        self.date = date
+    def __init__(self, start_date="2024-01-01"):
+        self.date = start_date
 
         self.pollution_level = 0            # FROM 0 TO 5
         self.government_sensitivity = 0     # FROM 0 TO 5
 
-    def next_day(self):
+    def game_over(self):
         return
 
 
 class Player:
-    def __init__(self, health=10, money=500):
+
+    def __init__(self, health=10, money=2000):
         self.health = health
         self.money = money
 
+        self.wage = 2000
         self.protests_attended = 0
         self.starving = False
 
     def stays_home(self):
-        return
+        self.health += 3
 
     def goes_to_work(self):
-        return
+        self.money += self.wage
 
     def goes_to_protest(self):
-        return
+        self.protests_attended += 1
 
     def goes_to_hospital(self):
-        return
+        self.health += 5
+        self.money -= self.wage
+
+    def is_alive(self):
+        return self.health > 0
+
 
 class Newspaper:
     def __init__(self, date, local_news, poster):
         self.date = date
         self.local_news = local_news
         self.poster = poster
+
 
